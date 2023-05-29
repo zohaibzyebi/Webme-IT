c9ac6281f60a9673fe63c236fc3ce834c76ee170
zohaibzyebi <zyebi553@gmail.com>
2023-05-30 00:53:43 +0500

    fix: ignored .next file

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "2fcb946563d8f1c5"; }
+/******/ 		__webpack_require__.h = function() { return "3acb15fd5334a40a"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
